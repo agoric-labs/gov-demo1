5af79df2b993b0c1b22bc6c29caf18816e350ce6
Dan Connolly <dckc@madmode.com>
2021-09-24 18:03:48 -0500

    build: SES, CapTP deps using snowpack

--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
-export const main = () => {};
+import '@agoric/install-ses';
+import { E, makeCapTP } from '@agoric/captp';
+
+export const main = () => 'Hello, World!';
+
+console.log({ E, makeCapTP });

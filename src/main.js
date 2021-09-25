4c48f711196f664f01ec8c52b18d53d39cc80bc7
Dan Connolly <dckc@madmode.com>
2021-09-24 23:35:22 -0500

    feat: Create Registrar UI

--- src/main.js
+++ src/main.js
@@ -1,29 +1,41 @@
 // @ts-check
 import '@agoric/install-ses';
 import { E, makeCapTP } from '@agoric/captp';
-import {
-  activateWebSocket,
-  deactivateWebSocket,
-  getActiveSocket,
-} from './utils/fetch-websocket.js';
+import { makePromiseKit } from '@agoric/promise-kit';
 
-export const main = () => {
-  const setup = () => {
+/**
+ * @param {{
+ *   show: (sel: string) => void,
+ *   hide: (sel: string) => void,
+ *   onClick: (sel: string, l: EventListener) => void,
+ *   busy: (sel: string, thunk: () => Promise<void>) => Promise<void>
+ *   getField: (sel: string) => string
+ *   setField: (sel: string, value: string) => void
+ * }} ui
+ * @param {*} net
+ */
+export const main = (
+  ui,
+  { activateWebSocket, deactivateWebSocket, getActiveSocket },
+) => {
+  const networkSetup = () => {
     let walletAbort;
     let walletDispatch;
 
     const otherSide = harden({
       needDappApproval: (_origin, _pet) => {
         console.log('need approval');
+        ui.show('#needDappApproval');
       },
       dappApproved: (_origin) => {
         console.log('approved');
+        ui.hide('#needDappApproval');
       },
     });
 
+    const { promise: board, resolve: boardR } = makePromiseKit();
+    const { promise: zoe, resolve: zoeR } = makePromiseKit();
     const onConnect = async () => {
-      debugger;
-
       const socket = getActiveSocket();
       const {
         abort: ctpAbort,
@@ -37,7 +49,9 @@ export const main = () => {
       walletAbort = ctpAbort;
       walletDispatch = ctpDispatch;
       const walletP = getBootstrap();
-      console.log('@@@got it!', { walletP });
+
+      boardR(E(walletP).getBoard());
+      zoeR(E(walletP).getZoe());
     };
 
     const onDisconnect = () => {
@@ -56,10 +70,43 @@ export const main = () => {
       onMessage,
     });
 
-    return deactivateWebSocket;
+    return { deactivateWebSocket, board, zoe };
   };
 
-  return harden({ setup });
+  const { board, zoe } = networkSetup();
+
+  const withCatch = (oops, go) => (ev) => go(ev).catch((err) => oops(err));
+  ui.onClick(
+    'form button#createRegistrar',
+    withCatch(
+      (err) => {
+        debugger;
+        console.error(err);
+      },
+      async (_ev) => {
+        const committeeTerms = {
+          committeeName: ui.getField('input[name="committeeName"]'),
+          committeeSize: BigInt(ui.getField('input[name="committeeSize"]')),
+        };
+        await ui.busy('body', async () => {
+          const installation = E(board).getValue(
+            ui.getField('input[name="registrarInstallation"]'),
+          );
+          const { creatorFacet, publicFacet } = await E(zoe).startInstance(
+            installation,
+            {},
+            committeeTerms,
+          );
+          const [creatorId, publicId] = await Promise.all([
+            E(board).getId(creatorFacet),
+            E(board).getId(publicFacet),
+          ]);
+          ui.setField('input[name="registrarCreatorFacet"]', creatorId);
+          ui.setField('input[name="registrarPublicFacet"]', publicId);
+        });
+      },
+    ),
+  );
 };
 
 console.log({ E, makeCapTP });

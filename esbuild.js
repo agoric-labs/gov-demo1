be937d75e04e5a7e8e1353cea80d5f8998d1baa0
Dan Connolly <dckc@madmode.com>
2022-09-21 01:18:57 -0500

    build: use esbuild

---
+++ esbuild.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+/* eslint-disable import/no-extraneous-dependencies */
+/* global require, process */
+import ESBuildNodePolyfillsPlugin from 'esbuild-plugin-node-polyfills';
+import esbuild from 'esbuild';
+
+// Your ESBuild Config
+const config = {
+  plugins: [ESBuildNodePolyfillsPlugin],
+  entryPoints: ['./src/install-ses-lockdown.js', './src/index.js'],
+  bundle: true,
+  outdir: 'dist/',
+  format: 'esm',
+  metafile: true,
+  target: 'esnext',
+};
+
+(async () => {
+  const result = await esbuild.build(config);
+  const text = await esbuild.analyzeMetafile(result.metafile);
+  console.log(text);
+})().catch(() => process.exit(1));

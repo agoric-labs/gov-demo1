a7a2ec30b7a92af16ddf0750affea8015f7fa118
Dan Connolly <dckc@madmode.com>
2022-09-21 01:19:44 -0500

    chore: fetch, keplr on window

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-/* global document, fetch, keplr */
+/* global document, window */
 import { makeUI, setupTabs } from './ui.js';
 import { main } from './main.js';
 
@@ -6,6 +6,6 @@ console.log('hi from index.js');
 
 const ui = makeUI(document);
 setupTabs(ui);
-main(ui, { fetch: (...args) => fetch(...args), kelpr })
+main(ui, { fetch: (...args) => window.fetch(...args), kelpr: window.keplr })
   .then(() => console.log('ready.'))
   .catch(err => console.error('main:', err));

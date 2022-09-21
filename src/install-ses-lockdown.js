fcb8ad6e5b44aa42b4635f7015c4e5e53c994bf5
Dan Connolly <dckc@madmode.com>
2022-09-21 01:17:22 -0500

    fixup! chore: ses/endo init flailing

--- src/install-ses-lockdown.js
+++ src/install-ses-lockdown.js
@@ -1,3 +1,5 @@
+import '@endo/eventual-send/shim.js';
+
 // Help lock down the JS environment.  The start compartment (current evaluation context)
 // can still access powerful globals, but this start compartment can use `new Compartment(...)`
 // to evaluate code with stricter confinement.
